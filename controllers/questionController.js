7a946f4e24c4075852924d7606f785922068157c
kcbobbe <kcbobbe@gmail.com>
2020-04-04 13:08:04 -0400

    linting

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -7,17 +7,17 @@ db.Question.sync();
 
 router.get("/", (req, res) => {
   db.Question.findAll({ raw: true }).then(data => {
-    res.render("index", { questions: (data) })
-  })
-})
+    res.render("index", { questions: data });
+  });
+});
 
 router.get("/api/questions/:id?", (req, res) => {
-  if(req.params.id) {
+  if (req.params.id) {
     db.Router.findAll({ raw: true }).then(data => {
-      res.json(data)
-    })
+      res.json(data);
+    });
   }
-})
+});
 
 router.post("/api/questions", (req, res) => {
   const newQ = {
@@ -25,16 +25,17 @@ router.post("/api/questions", (req, res) => {
     question_tag: req.body.question_tag,
     user_id: req.body.user_id,
     date: req.body.date
-  }
+  };
 
-  db.Question.create(newQ).then(data => {
-    res.status(200).end();
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).end()
-  })
-})
+  db.Question.create(newQ)
+    .then(data => {
+      res.status(200).end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
 
 router.put("/api/questions/:id", (req, res) => {
   const updQ = {
@@ -42,31 +43,32 @@ router.put("/api/questions/:id", (req, res) => {
     question_tag: req.body.question_tag,
     user_id: req.body.user_id,
     date: req.body.date
-  }
-db.Question.update(updQ, {
-  where: { id: req.params.id }
-})
-.then(function(data) {
-  res.status(200.end)
-  }).catch(err => {
-    console.log(err);
-    res.status(500).end();
-  });
+  };
+  db.Question.update(updQ, {
+    where: { id: req.params.id }
+  })
+    .then(function(data) {
+      res.status(200).end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
 });
 
 router.delete("/api/questions/:id", (req, res) => {
-db.Question.destroy({
-  where: {
-    id: req.params.id
-  }
-})
-  .then(function(data) {
-    res.status(200).end();
+  db.Question.destroy({
+    where: {
+      id: req.params.id
+    }
   })
-  .catch(err => {
-    console.log(err);
-    res.status(500).end();
-  });
+    .then(function(data) {
+      res.status(200).end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
 });
 
 module.exports = router;
