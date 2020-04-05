b5e7bc5b89ff3a13ae902686b129ec387d480223
Steffield <steffilebby@gmail.com>
2020-04-05 17:40:57 -0400

    get routes to index, login, signup handlebar

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,9 +8,23 @@ db.User.sync();
 
 //-------route to handlebar index file------
 
+//-----get routes----------
+
 router.get("/", (req, res) => {
   db.User.findAll({ raw: true }).then(data => {
     res.render("index", { users: data });
+  });
+});
+
+router.get("/login",(req, res)=>{
+  db.User.findAll({ raw: true }).then(data => {
+    res.render("login", { users: data });
+  });
+});
+
+router.get("/signup", (req, res)=>{
+  db.User.findAll({ raw: true }).then(data => {
+    res.render("signup", { users: data });
   });
 });
 
@@ -78,32 +92,32 @@ router.get("/api/user_data", function(req, res) {
 });
 // -----------------------------------------HTML Routes------------------------------------------------
 
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+// // Requiring path to so we can use relative routes to our HTML files
+// var path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+// // Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-router.get("/", function(req, res) {
-  // If the user already has an account send them to the members page
-  if (req.user) {
-    res.redirect("/members");
-  }
-  res.sendFile(path.join(__dirname, "../public/signup.html"));
-});
+// router.get("/", function(req, res) {
+//   // If the user already has an account send them to the members page
+//   if (req.user) {
+//     res.redirect("/members");
+//   }
+//   res.sendFile(path.join(__dirname, "../public/signup.html"));
+// });
 
-router.get("/login", function(req, res) {
-  // If the user already has an account send them to the members page
-  if (req.user) {
-    res.redirect("/members");
-  }
-  res.sendFile(path.join(__dirname, "../public/login.html"));
-});
+// router.get("/login", function(req, res) {
+//   // If the user already has an account send them to the members page
+//   if (req.user) {
+//     res.redirect("/members");
+//   }
+//   res.sendFile(path.join(__dirname, "../public/login.html"));
+// });
 
-// Here we've add our isAuthenticated middleware to this route.
-// If a user who is not logged in tries to access this route they will be redirected to the signup page
-router.get("/members", isAuthenticated, function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/members.html"));
-});
+// // Here we've add our isAuthenticated middleware to this route.
+// // If a user who is not logged in tries to access this route they will be redirected to the signup page
+// router.get("/members", isAuthenticated, function(req, res) {
+//   res.sendFile(path.join(__dirname, "../public/members.html"));
+// });
 
 module.exports = router;
