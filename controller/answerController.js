aa827eaa781d8dad0d32bb779096d231935e499a
Rashmi Sharma <rksharma2679@gmail.com>
2020-04-04 21:38:07 -0400

    Adding answerController

---
+++ controller/answerController.js
@@ -0,0 +1,86 @@
+const express = requirer("express");
+const router = express.Router();
+const db = require("../models");
+
+db.Answer.sync();
+
+router.get("/", (req, res) => {
+  db.Answer.findAll({ raw: true }).then(data => {
+    res.render("index", { answers: data });
+  });
+});
+
+router.get("/api/answers/:id?", (req, res) => {
+  if (req.params.id) {
+    db.Answer.findOne({
+      where: { id: req.params.id },
+      raw: true
+    })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        console.log(err);
+        status(500).end();
+      });
+  } else {
+    db.Answer.findAll({ raw: true }).then(data => {
+      res.json(data);
+    });
+  }
+});
+
+router.post("/api/answers", (req, res) => {
+  const newAnswer = {
+    answerText: req.body.answerText,
+    answerTag: req.body.answerTag,
+    date: req.body.date
+  };
+
+  db.Answer.create(newAnswer)
+    .then(data => {
+      res.status(200).end();
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+router.put("/api/answers/:id", (req, res) => {
+  const updAnswer = {
+    answerText: req.body.answerText,
+    answerTag: req.body.answerTag,
+    date: req.body.date
+  };
+  db.Answer.update(updAnswer, {
+    where: { id: req.params.id }
+  })
+    .then(function(data) {
+      res.status(200).end();
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+      console.log(data);
+    });
+});
+
+router.delete("/api/answers/:id", (req, res) => {
+  db.Answer.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(function(data) {
+      res.status(200).end();
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+module.exports = router;
