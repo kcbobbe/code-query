33a1fc5fe6f94cceef76d4284f601ed19ed66d86
kcbobbe <kcbobbe@gmail.com>
2020-04-06 13:36:00 -0400

    fixed answers and questions routes. Needed to 'use' both the answers and questions controller in the server.js file

--- models/question.js
+++ models/question.js
@@ -10,7 +10,9 @@ module.exports = function(sequelize, DataTypes) {
     questionTag: {
       type: DataTypes.STRING,
       allowNull: false,
-      len: [1]
+      validate: {
+        len: [1]
+      }
     }
     // the created at date and the updated at date is provided from sequelize. Eventually we should convert to date using moment
     // date: {
