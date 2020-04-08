298137a8644cffa0d9720ae074eec5b6fb11a1e7
Steffield <steffilebby@gmail.com>
2020-04-08 18:48:03 -0400

    Travil fail, pushing again

--- public/js/main.js
+++ public/js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 
     // $.get(url, function() {
 
-  //   //   //  ("index", { questions: data });
-  //   // });
+    //   //   //  ("index", { questions: data });
+    //   // });
   }
 });
