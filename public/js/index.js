f8baf5495bdae7c136dddc60d9216a862e150018
kcbobbe <kcbobbe@gmail.com>
2020-04-10 13:33:51 -0400

    hopefully fixed the issue with the textboxes throwing an error and repeating

--- public/js/index.js
+++ public/js/index.js
@@ -14,24 +14,24 @@ $(document).ready(function() {
       $("#askQuestion").addClass("hide");
       $(".replyButton").addClass("hide");
     }
-    $(".memberSection").click(function() {
-      console.log("member clicked");
-      //need to get userID of current user
-      const CurrentUser = req.id;
-      console.log(CurrentUser + "clicked");
-      renderMemberQuestion(CurrentUser);
-    });
-    $(".userUsername").click(function(event) {
-      console.log(event.target);
-      // let user = event.target.UserId;
-      // console.log(user + "clicked");
-      // renderMemberQuestion(user);
-    });
-    function renderMemberQuestion(UserId) {
-      const url = "/member/" + UserId;
-      console.log(url);
-      window.location.href = url;
-      $("#askQuestion").css("display", "none");
-    }
+    // $(".memberSection").click(function() {
+    //   console.log("member clicked");
+    //   //need to get userID of current user
+    //   const CurrentUser = req.id;
+    //   console.log(CurrentUser + "clicked");
+    //   renderMemberQuestion(CurrentUser);
+    // });
+    // $(".userUsername").click(function(event) {
+    //   console.log(event.target);
+    //   // let user = event.target.UserId;
+    //   // console.log(user + "clicked");
+    //   // renderMemberQuestion(user);
+    // });
+    // function renderMemberQuestion(UserId) {
+    //   const url = "/member/" + UserId;
+    //   console.log(url);
+    //   window.location.href = url;
+    //   $("#askQuestion").css("display", "none");
+    // }
   });
 });
