06ff88ec680de7baaf14e4f3cd8a875fb3c6bef8
Steffield <steffilebby@gmail.com>
2020-04-09 22:12:52 -0400

    getting to your logged in member section with your questions works

--- public/js/index.js
+++ public/js/index.js
@@ -14,5 +14,23 @@ $(document).ready(function() {
       $("#askQuestion").css("display", "none");
       $(".replyButton").css("display", "none");
     }
+    $(".memberSection").click(function() {
+      console.log("member clicked");
+      //need to get userID of current user
+      const CurrentUser = req.id;
+      console.log(CurrentUser + "clicked");
+      renderMemberQuestion(CurrentUser);
+    });
+    $(".userUsername").click(function(event) {
+      console.log(event.target);
+      // let user = event.target.UserId;
+      // console.log(user + "clicked");
+      // renderMemberQuestion(user);
+    });
+    function renderMemberQuestion(UserId) {
+      const url = "/member/" + UserId;
+      console.log(url);
+      window.location.href = url;
+    }
   });
 });
